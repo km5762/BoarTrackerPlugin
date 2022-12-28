a059575076825eaecf8fd6f8c9a4a4f9d83771d9
Myles Ku <mcku@wpi.edu>
2022-12-28 14:06:05 -0500

    Finished

---
+++ BoarPlugin/BoarPlugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
+
+
+namespace BoarTrackerPlugin
+{
+    public class BoarPlugin : IPlugin
+    {
+        public string Author
+        {
+            get { return "km5762"; }
+        }
+
+        public string ButtonText
+        {
+            get { return "Settings"; }
+        }
+
+        public string Description
+        {
+            get { return "A simple plugin to track and display the number of your Elwynn Boars that have died."; }
+        }
+
+        public MenuItem MenuItem
+        {
+            get { return null; }
+        }
+
+        public string Name
+        {
+            get { return "Boar Tracker"; }
+        }
+
+        public void OnButtonPress()
+        {
+        }
+
+        public void OnLoad()
+        {
+            BoarDisplay _display = new BoarDisplay();
+            //_display.Show();
+            //BoarTracker bt = new BoarTracker();
+
+            //GameEvents.OnGameStart.Add(bt.GameStart);
+            //GameEvents.OnInMenu.Add(bt.InMenu);
+            //GameEvents.OnTurnStart.Add(bt.TurnStart);
+        }
+
+        public void OnUnload()
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public Version Version
+        {
+            get { return new Version(0, 1, 1); }
+        }   
+    }
+}
---
+++ BoarPlugin/BoarTracker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Enums;
+
+namespace BoarTrackerPlugin
+{
+    internal class BoarTracker
+    {
+        private int _boarCount = 0;
+
+        public BoarTracker()
+        {
+        }
+
+        internal void GameStart()
+        {
+        }
+
+        internal void InMenu()
+        {
+        }
+
+        internal void TurnStart(ActivePlayer player)
+        {
+        }
+    }
+}
